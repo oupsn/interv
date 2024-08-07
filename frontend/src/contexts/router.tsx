1b5af8ded2b1be058dc2b38498648358095a60dc
wuhoops <pasinun.w@gmail.com>
2024-08-07 22:23:27 +0700

    lint

--- frontend/src/contexts/router.tsx
+++ frontend/src/contexts/router.tsx
@@ -5,7 +5,6 @@ import LoginPage from "@/pages/login/Login.tsx"
 import LobbyPage from "@/pages/lobby/Lobby.tsx"
 import { Playground } from "@/pages/playground/Playground.tsx"
 import Dashboard from "@/pages/dashboard/Dashboard.tsx"
-import LobbyLayout from "@/components/layout/LobbyLayout.tsx"
 import CodingInterviewPage from "@/pages/lobby/codingInterview/CodingInterview.tsx"
 import VideoInterviewPage from "@/pages/lobby/videoInterview/VideoInterview.tsx"
 
