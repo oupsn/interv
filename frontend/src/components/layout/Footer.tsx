a546470b8fee08dc70e5037509291224b0fe190e
Mr.PHAWIT MONCHAISING <phawit.monc@kmutt.ac.th>
2024-10-24 12:51:30 +0700

    [UPDATE] Panel footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-4 md:mx-8 flex h-14 items-center">
         <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
-          Made with stress and anxiety ❤️
+          Copyright © 2024 Interv.cc
         </p>
       </div>
     </div>
