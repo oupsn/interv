0e8f19d639bddc7014d02f5b8fe2fe780abde691
wuhoops <95624019+wuhoops@users.noreply.github.com>
2024-10-29 16:23:25 +0700

    handle retry

--- frontend/src/pages/room/videoInterview/components/VideoInterviewPostQuestion.tsx
+++ frontend/src/pages/room/videoInterview/components/VideoInterviewPostQuestion.tsx
@@ -90,13 +90,20 @@ export const VideoInterviewPostQuestion: FC<VideoInterviewPostQuestion> = ({
       </div>
 
       <p className={"text-xl font-semibold"}>
-        You have {attemptLeft} attempts left.
+        You have{" "}
+        {attemptLeft - Number(Cookies.get("r_" + questionId.toString()))}{" "}
+        attempts left.
       </p>
       <div className={"flex gap-10"}>
-        {attemptLeft > 0 ? (
+        {attemptLeft - Number(Cookies.get("r_" + questionId.toString())) > 0 ? (
           <Button
             onClick={() => {
               setRecordState("detail")
+              const currentAttempt = Cookies.get("r_" + questionId.toString())
+              Cookies.set(
+                "r_" + questionId.toString(),
+                String(Number(currentAttempt ?? 0) + 1),
+              ) //TODO: come back here one day
             }}
           >
             Retake
