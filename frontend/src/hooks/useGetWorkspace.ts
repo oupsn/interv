e2803dbd598ecb2cd1ce1c831f88df873f881a53
wuhoops <pasinun.w@gmail.com>
2024-11-03 22:54:23 +0700

    fix all persist data SWR

--- frontend/src/hooks/useGetWorkspace.ts
+++ frontend/src/hooks/useGetWorkspace.ts
@@ -2,7 +2,7 @@ import useSWR from "swr"
 import { server } from "@/contexts/swr.tsx"
 
 export const useGetWorkspace = (id: number) => {
-  return useSWR(["workspace", "getWorkspace"], () =>
+  return useSWR(["workspace", "getWorkspace", id], () =>
     server.workspace.getWorkspace({ id: id }),
   )
 }
