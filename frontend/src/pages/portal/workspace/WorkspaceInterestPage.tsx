13dd2bbd3f2dd2e2733636370096fc5634bcfa65
Phatdanai Shinpanjapol <petersafe45@gmail.com>
2024-11-06 13:09:43 +0700

    fix lint

--- frontend/src/pages/portal/workspace/WorkspaceInterestPage.tsx
+++ frontend/src/pages/portal/workspace/WorkspaceInterestPage.tsx
@@ -17,12 +17,17 @@ import { ContentLayout } from "@/components/layout/ContentLayout.tsx"
 import Panigator from "./components/Panigator"
 import { Spinner } from "@/components/ui/spinner"
 import { toast } from "sonner"
+import useCurrentUser from "@/hooks/UseCurrentUser"
 
 const WorkspaceInterestPage = () => {
   const [page, setPage] = useState(1)
+  const { currentUser } = useCurrentUser()
   const size = 10
   const { workspaceId } = useParams()
-  const { data, isLoading } = useGetWorkspace(Number(workspaceId))
+  const { data, isLoading } = useGetWorkspace(
+    Number(workspaceId),
+    Number(currentUser.portalId),
+  )
   const interest = data?.data
     ? data?.data?.userInWorkspace?.filter((candidate) => candidate.isInterest)
     : []
