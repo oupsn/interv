00dc4aa5e9d70d0640e8871684f26a1f50dff72d
peem <phawit.monc@kmutt.ac.th>
2024-11-07 05:58:06 +0000

    [INT-105] Change plagiarism algorithm, Coding time counter

--- frontend/src/components/layout/TopBar.tsx
+++ frontend/src/components/layout/TopBar.tsx
@@ -1,37 +1,36 @@
 import { FC, ReactNode } from "react"
 import IntervLogo from "@/assets/interv-logo.png"
 import { Button } from "@/components/ui/button.tsx"
+import TopBarItem from "./TopBarItem"
 
 interface TopBarProps {
   children?: ReactNode
-  isCodingInterview?: boolean
+  timeRemain?: string
 }
 
-const TopBar: FC<TopBarProps> = ({ children, isCodingInterview }) => {
+const TopBar: FC<TopBarProps> = ({ children, timeRemain }) => {
   const handleReportIssue = () => {
     window.location.href = "mailto:interv.cc"
   }
 
   return (
     <div className="flex flex-row justify-between w-full outline outline-1 outline-gray-200 px-16 items-center">
-      <div className={"flex gap-10"}>
+      <div className={"flex gap-10 flex-row items-center justify-between"}>
         <img src={IntervLogo} alt="interv" className="w-28 mb-2" />
-        <div
-          style={{
-            justifyContent: isCodingInterview ? "end" : "start",
-            marginTop: isCodingInterview ? "0px" : "16px",
-          }}
-        >
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
-      <Button
-        variant={"outline"}
-        onClick={handleReportIssue}
-        className="text-red-500 hover:text-red-500 text-sm font-semibold text-nowrap border border-red-500 rounded-md px-4"
-      >
-        Report Issue
-      </Button>
+      <div className="flex flex-row gap-4 items-center">
+        {timeRemain !== undefined && timeRemain !== "" && (
+          <TopBarItem title={"Time Remaining: " + timeRemain} />
+        )}
+        <Button
+          variant={"outline"}
+          onClick={handleReportIssue}
+          className="text-red-500 hover:text-red-500 text-sm font-semibold text-nowrap border border-red-500 rounded-md px-4"
+        >
+          Report Issue
+        </Button>
+      </div>
     </div>
   )
 }
